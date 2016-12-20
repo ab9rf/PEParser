a784e4a13e34592acb897839a8ff067a64184580
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2016-12-20 03:37:41 -0600

    Rearrange

--- src/PEParser.hs
+++ src/PEParser.hs
@@ -16,6 +16,8 @@ getPEFile filename = do
                         0x4E50 -> Just 0
                         _      -> Nothing
          in return $ fmap (readPEFile bytes) offset
+    where
+        getFileContents filename = mmapFileByteStringLazy filename Nothing
 
 readPEFile bytes offset = runGet r bytes
     where r :: Get PEFile
@@ -29,7 +31,43 @@ readPEFile bytes offset = runGet r bytes
                 let pe = PEFile signature coffheader optHeader
                   in return pe
 
-getFileContents filename = mmapFileByteStringLazy filename Nothing
+-----------------------------------------------------------------
+
+data PEMachine = PEM_Any | PEM_AM33 | PEM_AMD64 | PEM_ARM |
+    PEM_ARMNT | PEM_EBC | PEM_I386 | PEM_IA64 | PEM_M32R | PEM_MIPS16 |
+    PEM_MIPSFPU | PEM_MIPSFPU16 | PEM_POWERPC | PEM_POWERPCFP |
+    PEM_R4000 | PEM_RISCV32 | PEM_RISCV64 | PEM_RISCV128 |
+    PEM_SH3 | PEM_SH3DSP | PEM_SH4 | PEM_SH5 | PEM_THUMB | PEM_WCEMIPSV2 |
+    PEM_Unknown Word16
+    deriving (Show, Eq)
+
+peMachine 0x0 = PEM_Any
+peMachine 0x1d3 = PEM_AM33
+peMachine 0x8664 = PEM_AMD64
+peMachine 0x1c0 = PEM_ARM
+peMachine 0x1c4 = PEM_ARMNT
+peMachine 0xebc = PEM_EBC
+peMachine 0x14c = PEM_I386
+peMachine 0x200 = PEM_IA64
+peMachine 0x9041 = PEM_M32R
+peMachine 0x266 = PEM_MIPS16
+peMachine 0x366 = PEM_MIPSFPU
+peMachine 0x466 = PEM_MIPSFPU16
+peMachine 0x1f0 = PEM_POWERPC
+peMachine 0x1f1 = PEM_POWERPCFP
+peMachine 0x166 = PEM_R4000
+peMachine 0x5032 = PEM_RISCV32
+peMachine 0x5064 = PEM_RISCV64
+peMachine 0x5128 = PEM_RISCV128
+peMachine 0x1a2 = PEM_SH3
+peMachine 0x1a3 = PEM_SH3DSP
+peMachine 0x1a6 = PEM_SH4
+peMachine 0x1a8 = PEM_SH5
+peMachine 0x1c2 = PEM_THUMB
+peMachine 0x169 = PEM_WCEMIPSV2
+peMachine i = PEM_Unknown i
+
+-----------------------------------------------------------------
 
 data PEFile = PEFile {
       signature :: B.ByteString
@@ -114,39 +152,7 @@ data OptionalHeader =
     }
     deriving (Show, Eq)
 
-data PEMachine = PEM_Any | PEM_AM33 | PEM_AMD64 | PEM_ARM |
-    PEM_ARMNT | PEM_EBC | PEM_I386 | PEM_IA64 | PEM_M32R | PEM_MIPS16 |
-    PEM_MIPSFPU | PEM_MIPSFPU16 | PEM_POWERPC | PEM_POWERPCFP |
-    PEM_R4000 | PEM_RISCV32 | PEM_RISCV64 | PEM_RISCV128 |
-    PEM_SH3 | PEM_SH3DSP | PEM_SH4 | PEM_SH5 | PEM_THUMB | PEM_WCEMIPSV2 |
-    PEM_Unknown Word16
-    deriving (Show, Eq)
-
-peMachine 0x0 = PEM_Any
-peMachine 0x1d3 = PEM_AM33
-peMachine 0x8664 = PEM_AMD64
-peMachine 0x1c0 = PEM_ARM
-peMachine 0x1c4 = PEM_ARMNT
-peMachine 0xebc = PEM_EBC
-peMachine 0x14c = PEM_I386
-peMachine 0x200 = PEM_IA64
-peMachine 0x9041 = PEM_M32R
-peMachine 0x266 = PEM_MIPS16
-peMachine 0x366 = PEM_MIPSFPU
-peMachine 0x466 = PEM_MIPSFPU16
-peMachine 0x1f0 = PEM_POWERPC
-peMachine 0x1f1 = PEM_POWERPCFP
-peMachine 0x166 = PEM_R4000
-peMachine 0x5032 = PEM_RISCV32
-peMachine 0x5064 = PEM_RISCV64
-peMachine 0x5128 = PEM_RISCV128
-peMachine 0x1a2 = PEM_SH3
-peMachine 0x1a3 = PEM_SH3DSP
-peMachine 0x1a6 = PEM_SH4
-peMachine 0x1a8 = PEM_SH5
-peMachine 0x1c2 = PEM_THUMB
-peMachine 0x169 = PEM_WCEMIPSV2
-peMachine i = PEM_Unknown i
+-----------------------------------------------------------------
 
 getCOFFHeader :: Get COFFHeader
 getCOFFHeader = COFFHeader
@@ -224,3 +230,5 @@ getOptionalHeader = do magic <- getWord16le
                                         <*> getWord64le         -- SizeOfHeapCommit
                                         <*> getWord32le         -- LoaderFlags
                                         <*> getWord32le         -- NumberOfRvaAndSizes
+
+
